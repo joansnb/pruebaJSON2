105c669103252b1c6219b399940c3d362b3fa978
joansnb <joansnb@gmail.com>
2023-12-30 14:37:28 +0100

    Codigo extra para imprimir habilidades

--- src/main/java/MiLectorJson.java
+++ src/main/java/MiLectorJson.java
@@ -1,3 +1,4 @@
+import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,6 +17,14 @@ public class MiLectorJson {
             // Acceder al elemento "nombre" y mostrarlo:
             String nombre = objetoJSON.getString("nombre");
             System.out.println("El nombre en el JSON es: " + nombre);
+
+            //codigo para imprimir habilidades
+            JSONArray habilidades = objetoJSON.getJSONArray("habilidades");
+            System.out.println("Habilidades de " + nombre + ":");
+            for (int i = 0; i < habilidades.length(); i++) {
+                System.out.println("- " + habilidades.getString(i));
+            }
+
 
         } catch (IOException e) {
             System.out.println("Error leyendo el archivo: " + e.getMessage());
