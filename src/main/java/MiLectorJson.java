7aa1b980159b182db3649786ed82a56977d2f124
joansnb <joansnb@gmail.com>
2023-12-30 14:22:29 +0100

    Agregando codigo clase MiLectorJson

--- src/main/java/MiLectorJson.java
+++ src/main/java/MiLectorJson.java
@@ -1,2 +1,23 @@
+import org.json.JSONObject;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class MiLectorJson {
+
+    public static void main(String[] args) {
+        String filePath = "src/main/java/MiLectorJson.java"; // ruta real de archivo JSON
+
+        try {
+            String content = new String(Files.readAllBytes(Paths.get(filePath)));
+            JSONObject objetoJSON = new JSONObject(content);
+
+            // Acceder al elemento "nombre" y mostrarlo:
+            String nombre = objetoJSON.getString("nombre");
+            System.out.println("El nombre en el JSON es: " + nombre);
+
+        } catch (IOException e) {
+            System.out.println("Error leyendo el archivo: " + e.getMessage());
+        }
+    }
 }
